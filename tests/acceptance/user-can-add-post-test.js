4d3cc018a0a12331e388adabd6f0d4df7ce45904
Stuart Quin <stuart.quin@gmail.com>
2015-07-26 14:37:45 +0100

    Add some tests

--- tests/acceptance/user-can-add-post-test.js
+++ tests/acceptance/user-can-add-post-test.js
@@ -16,8 +16,12 @@ module('Acceptance | user can add post', {
 
 test('visiting /user-can-add-post', function(assert) {
   visit('/posts');
+  fillIn('#title', 'Test Post');
+  fillIn('#content', 'Test content');
+  click('.create');
 
   andThen(function() {
     assert.equal(currentURL(), '/posts');
+    assert.equal(find("article").length, 3);
   });
 });
